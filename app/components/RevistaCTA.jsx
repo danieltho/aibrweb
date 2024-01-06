248f0737ff8cff2cb4a7b0eb9a322aa5aa60bbeb
Daniel Thomann <g.danieltho@gmail.com>
2024-01-06 11:39:36 -0300

    agrego fonts local y correcciones de seccion revista

--- app/components/RevistaCTA.jsx
+++ app/components/RevistaCTA.jsx
@@ -1,28 +1,27 @@
-import {StyledText} from "./StyledText";
-import {Button} from "./Button";
 import Image from "next/image";
 import exampleImage from "../../public/assets/images/image_34.png";
+import {Link, Button} from "@nextui-org/react";
+import {gothic_a1, lato, libre_franklin, trade_gothic} from "../ui/fonts";
 
 export const RevistaCTA = ({...props}) => {
     return (<div className="section-default">
-        <div className="container flex flex-col gap-2 m-auto">
-            <StyledText size={`xl`} text={`AIBR, Revista de Antropología Iberoamericana`} />
-            <div className={`flex justify-start items-center gap-2`}>
-                <StyledText size={`md`} text={`Números recientes.`} />
-                <div className={`flex gap-1`}>
-                    <Button label={`Archivo`}  />
-                    <Button label={`Consejo`}  />
-                    <Button label={`Indices`}  />
-                    <Button label={`Manuscritos`}  />
-                    <Button label={`Autores`}  />
+        <div className={`container m-auto`}>
+            <div className="flex flex-col gap-4 xl:gap-x-8 ">
+                <h2 className={`${lato.className} antialiased font-bold text-4xl md:text-5xl text-balance leading-none  text-center sm:text-left`}>
+                    <span className={`${trade_gothic.className} tracking-tighter`}>AIBR</span>, Revista de Antropología Iberoamericana</h2>
+                <div className={`flex justify-start items-center gap-2 justify-center sm:justify-start`}>
+                    <p className={`${lato.className} antialiased font-light text-lg md:text-2xl text-pretty leading-normal`}>Números
+                        recientes</p>
+
                 </div>
-            </div>
-            <div className={`grid grid-cols-5 gap-1`}>
-                <Image src={exampleImage} width={260} height={`auto`} alt={``} />
-                <Image src={exampleImage} width={260} height={`auto`} alt={``} />
-                <Image src={exampleImage} width={260} height={`auto`} alt={``} />
-                <Image src={exampleImage} width={260} height={`auto`} alt={``} />
-                <Image src={exampleImage} width={260} height={`auto`} alt={``} />
+                <div className={`flex flex-wrap gap-4 justify-center sm:justify-start`}>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                    <Image src={exampleImage} width={234} height={`auto`} alt={``}/>
+                </div>
             </div>
         </div>
     </div>)
--- app/ui/fonts.js
+++ app/ui/fonts.js
@@ -1,8 +1,21 @@
-import { Inter,
+import {
+    Inter,
     Lato as LatoFont,
-    Open_Sans
+    Open_Sans,
+    Gothic_A1,
+    Libre_Franklin
 } from 'next/font/google';
-export const inter = Inter({ subsets: ['latin']});
-export const lato = LatoFont({ subsets: ['latin'], weight: ["300","400","700"] });
-export const open_sans = Open_Sans({ subsets: ['latin']});
+import localFont from 'next/font/local'
+
+export const inter = Inter({subsets: ['latin']});
+export const lato = LatoFont({subsets: ['latin'], weight: ["300", "400", "700"]});
+export const open_sans = Open_Sans({subsets: ['latin']});
+export const gothic_a1 = Gothic_A1({subsets: ['latin'], weight: ["300", "400", "700"]});
+export const libre_franklin = Libre_Franklin({subsets: ['latin'], weight: ["300", "400", "700"]});
+
+export const trade_gothic = localFont({
+    src: './fonts/TradeGothic-Bold.woff2',
+    display: 'swap',
+});
+
 
